ca2d178d17179232aaaf31b777d8d3bf399e9d32
IvanTsegelnyuk <tsegelnyuk.ivan@gmail.com>
2024-01-19 02:45:35 +1000

    add method getClosetToZero()

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,10 +1,14 @@
 package model;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         String input = "ПппппппппПППппппррррррРРрррррррииВееееееееЕЕЕеетттттттт!!!!!";
         System.out.println(test(input));
+
+        System.out.println(getClosestToZero(List.of(9, 3, -5, 4, -4, 7, 3, 11, -3, -2, -1)));
     }
 
 
@@ -21,5 +25,19 @@ public class Main {
             }
         }
         return sb.toString();
+    }
+
+    public static int getClosestToZero(final List<Integer> ints) {
+        // 9, 3, -5, 4, -4, 7, 3, 11, -3 -> 3
+        int prevResult = ints.get(0);
+        int result = ints.get(0); //-3
+        for (int i = 1; i < ints.size(); i++) {
+            if (Math.abs(ints.get(i)) <= Math.abs(result)) {
+                prevResult = result;
+                result = ints.get(i);
+            }
+        }
+        if (Math.abs(result) < Math.abs(prevResult)) return result;
+        else return result > prevResult ? result : prevResult;
     }
 }
